4699e689d7ce9274ce4482ed1e321adc3dac5d85
Jarek Páral <paral.jarek@gmail.com>
2017-05-16 19:33:18 +0200

    Install script

---
+++ RB3rt-install.bat
@@ -0,0 +1,32 @@
+set "origin=%cd%"
+
+set "tmp=%Temp%\robotikabrno-lego"
+if not exist "%tmp%" mkdir %tmp%"
+cd %tmp%
+
+set "wget=%tmp%\wget.exe"
+bitsadmin.exe /transfer "Downloading wget" https://eternallybored.org/misc/wget/current/wget64.exe %wget%
+
+%wget% http://www.7-zip.org/a/7z1604-x64.exe -O 7z-setup.exe
+7z-setup.exe /S
+set "unzip=C:\Program Files\7-Zip\7z"
+
+%wget% https://go.microsoft.com/fwlink/?LinkID=623230 -O vscode.exe
+vscode.exe /SILENT
+set "vscode=C:\Program Files (x86)\Microsoft VS Code\bin\code"
+"%vscode%" --install-extension ms-vscode.cpptools
+
+%wget% https://cygwin.com/setup-x86_64.exe -O cygwin.exe
+cygwin.exe -q -s ftp://ftp-stud.hs-esslingen.de/pub/Mirrors/sources.redhat.com/cygwin/ -P make diffutils-3.5-2 perl git gcc-core gcc-g++ libboost-devel curl 
+
+wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-win32.exe -O toolchain.exe
+toolchain.exe /S
+
+wget https://bitbucket.org/Tasssadar/lorris_binaries/raw/b482b371fe2e08a28c1144563cde06558586bf66/Lorris.zip -O Lorris.zip
+"%unzip%" x Lorris.zip -oC:\Lorris
+
+wget http://files.robotikabrno.cz/RB3rt.zip
+"%unzip%" x RB3rt.zip -oC:\RB3rt
+
+cd %origin%
+@RD /S /Q %tmp%
