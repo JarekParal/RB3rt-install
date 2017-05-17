97decf3e6138c5009522db0b408797b280e5b0b4
Jarek Páral <paral.jarek@gmail.com>
2017-05-17 03:13:20 +0200

    Update install script

--- RB3rt-install.bat
+++ RB3rt-install.bat
@@ -1,6 +1,6 @@
 set "origin=%cd%"
 
-set "tmp=%Temp%\robotikabrno-lego"
+set "tmp=temp-rb3rt-install"
 if not exist "%tmp%" mkdir %tmp%"
 cd %tmp%
 
@@ -14,21 +14,19 @@ set "unzip=C:\Program Files\7-Zip\7z"
 %wget% https://go.microsoft.com/fwlink/?LinkID=623230 -O vscode.exe
 vscode.exe /SILENT
 set "vscode=C:\Program Files (x86)\Microsoft VS Code\bin\code"
-"%vscode%" --install-extension ms-vscode.cpptools
+cmd /d "%vscode%" --install-extension ms-vscode.cpptools
 
 %wget% https://cygwin.com/setup-x86_64.exe -O cygwin.exe
-cygwin.exe -q -s http://www.gutscheinrausch.de/mirror/cygwin/ -P make diffutils-3.5-2 perl git gcc-core gcc-g++ libboost-devel curl 
+cygwin.exe -q -s http://www.gutscheinrausch.de/mirror/cygwin/ -P make,diffutils-3.5-2,perl,git,gcc-core,gcc-g++,libboost-devel,curl 
 
-wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-win32.exe -O toolchain.exe
-toolchain.exe /S
+wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-win32.exe -O gcc-arm-none-eabi-toolchain.exe
+gcc-arm-none-eabi-toolchain.exe 
 
 wget https://bitbucket.org/Tasssadar/lorris_binaries/raw/b482b371fe2e08a28c1144563cde06558586bf66/Lorris.zip -O Lorris.zip
 "%unzip%" x Lorris.zip -oC:\Lorris
 
 wget http://files.robotikabrno.cz/RB3rt.zip
 "%unzip%" x RB3rt.zip -oC:\
-
-xcopy /y Make.bat C:\RB3rt\
 
 cd %origin%
 @RD /S /Q %tmp%
